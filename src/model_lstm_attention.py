0c0e6e5143e6b01a3dfee152498cc0453886b542
julius <zimmermann.julius@web.de>
2019-12-13 11:40:08 +0100

    update libraries, add lstm model with normalization layers;

--- src/model_lstm_attention.py
+++ src/model_lstm_attention.py
@@ -3,17 +3,25 @@ import numpy as np
 
 
 input = pd.read_csv('../data/complete.csv')
-texts = input['sentence'].values
+texts = input['sentence']
 labels = input['label'].values
 
 # preprocessing text
 stop_list = 'the to and i a of that is in you for it have he my with was are on but be this so not'.split()
 
+def clean_text(text, stoplist):
+    for word in stoplist:
+        text = text.replace(word, '')
+    return text
+
+texts = texts.apply(lambda x: clean_text(x, stop_list))
+texts = texts.values
+
 # tokenization
 from keras.preprocessing.text import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
 max_words = 10000
-max_length = 75
+max_length = 60
 tokenizer = Tokenizer(num_words=max_words)
 tokenizer.fit_on_texts(texts)
 sequences = tokenizer.texts_to_sequences(texts)
@@ -50,40 +58,48 @@ class_weight_dict = generate_balanced_weights(y_train)
 print(class_weight_dict)
 
 # model
-from keras.layers import Input, Embedding, LSTM, Dense, Bidirectional
-from keras.models import Model
+from tensorflow.keras.layers import Input, Embedding, LSTM, Dense, Bidirectional, BatchNormalization, Attention, Add, Flatten, Multiply
+from tensorflow.keras.models import Model
 
 features = 200
 input_1 = Input(shape=(max_length,))
 embed_1 = Embedding(input_dim=(max_words - 1), output_dim=features, input_length=max_length)(input_1)
-bi_lstm_1 = Bidirectional(LSTM(units=32, activation='tanh', dropout=0.2, return_sequences=True))(embed_1)
-bi_lstm_2 = Bidirectional(LSTM(units=32, activation='tanh', dropout=0.2, return_sequences=True))(bi_lstm_1)
-bi_lstm_3 = Bidirectional(LSTM(units=16, activation='tanh', dropout=0.2, return_sequences=False))(bi_lstm_2)
-softmax_1 = Dense(units=classes, activation='softmax')(bi_lstm_3)
+bi_lstm_1 = Bidirectional(LSTM(units=64, activation='tanh', dropout=0.2, return_sequences=True))(embed_1)
+b_norm_1 = BatchNormalization()(bi_lstm_1)
+bi_lstm_2 = Bidirectional(LSTM(units=64, activation='tanh', dropout=0.2, return_sequences=True))(b_norm_1)
+# add_1 = Multiply()([b_norm_1, bi_lstm_2])
+b_norm_2 = BatchNormalization()(bi_lstm_2)
+bi_lstm_3 = Bidirectional(LSTM(units=64, activation='tanh', dropout=0.2, return_sequences=True))(b_norm_2)
+add_2 = Multiply()([bi_lstm_1, bi_lstm_2, bi_lstm_3, b_norm_2])
+# attention_1 = Attention()([bi_lstm_3, bi_lstm_1])
+b_norm_3 = BatchNormalization()(add_2)
+flat_1 = Flatten()(b_norm_3)
+dense_1 = Dense(units=24, activation='sigmoid')(flat_1)
+softmax_1 = Dense(units=classes, activation='softmax')(dense_1)
 
 model = Model(inputs=input_1, outputs=softmax_1)
-model.compile(loss='categorical_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
 print(model.summary())
 
 # show model
-# from keras.utils.vis_utils import plot_model
-# plot_model(model, to_file='../images/model_plot_lstm.png', show_shapes=True, show_layer_names=True)
+# from tensorflow.keras.utils import plot_model
+# plot_model(model, to_file='../images/model_plot_lstm_norm_multi.png', show_shapes=True, show_layer_names=True)
 
 # training
 model.fit(x=x_train, y=y_train, validation_data=(x_val, y_val), batch_size=16, epochs=20, class_weight=class_weight_dict)
 
 # explain predictions
-import seaborn as sn
-from sklearn.metrics import classification_report, confusion_matrix
+# import seaborn as sn
+# from sklearn.metrics import classification_report, confusion_matrix
 
 # calculate confusion matrix
-y = [np.argmax(v) for v in y_val]
-x = [np.argmax(x) for x in model.predict(x_val)]
-confusion = confusion_matrix(y, x)
-classification = classification_report(y, x)
-print(confusion)
-print(classification)
+# y = [np.argmax(v) for v in y_val]
+# x = [np.argmax(x) for x in model.predict(x_val)]
+# confusion = confusion_matrix(y, x)
+# classification = classification_report(y, x)
+# print(confusion)
+# print(classification)
 
 # print confusion matrix
 # import matplotlib.pyplot as plt
