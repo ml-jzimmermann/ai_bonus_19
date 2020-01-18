6bfb41256436ca5acf43417354a16faed83d93db
Julius Zimmermann <zimmermann.julius@web.de>
2020-01-18 15:09:21 +0100

    refresh dataset;

--- src/dataset.py
+++ src/dataset.py
@@ -46,9 +46,6 @@ t.fit_on_texts(texts)
 print(texts[42])
 sequences = t.texts_to_sequences(texts)
 print(sequences[42])
-most_frequent = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55]
-most_frequent = t.sequences_to_texts([most_frequent])
-print(most_frequent)
 
 
 lengths = [len(text) for text in sequences]
